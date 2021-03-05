f4549896eeb71044cc344541735d90ae08220726
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-03-05 06:06:20 -0600

    Add basic generics test

---
+++ MonoMod.UnitTest/RuntimeDetour/GenericsTest.cs
@@ -0,0 +1,40 @@
+﻿using MonoMod.RuntimeDetour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MonoMod.UnitTest {
+    public class GenericsTest {
+        [Fact]
+        public void TestGenerics() {
+
+            int handle = DetourHelper.Generic.AddPatch(
+                typeof(GenericsTest).GetMethod(nameof(From), BindingFlags.NonPublic | BindingFlags.Static),
+                typeof(GenericsTest).GetMethod(nameof(To), BindingFlags.NonPublic | BindingFlags.Static));
+
+            try {
+                Wrapper();
+            } finally {
+                DetourHelper.Generic.RemovePatch(handle);
+            }
+        }
+
+        private static void Wrapper() {
+            From("hello");
+            From(42);
+        }
+
+        // TODO: test all the other cases
+        private static void From<T>(T value) {
+            Assert.True(false, "Original generic was called when it shouldn't have been!");
+        }
+
+        private static void To<T>(T value) {
+            Assert.True(true); // all is well
+        }
+    }
+}
